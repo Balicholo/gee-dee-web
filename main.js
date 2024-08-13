9187f32ab08d00a655a01f044b3666a94f5fe380
Balicholo <wisdombalicholo@gmail.com>
2024-08-13 11:04:30 +0200

    fifth

--- main.js
+++ main.js
@@ -75,3 +75,39 @@ sr.reveal(`.team-images`, {origin: 'bottom'})
 sr.reveal(`.mission-content, .cleaning-pic, .content`, {origin: 'left'})
 sr.reveal(`.portfolio-grid, .logo, .read-more-btn`, {origin: 'right'})
 sr.reveal(`.icon-set, .testimony-1`, {interval: 200})
+
+// DARKMODE
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector("#dark-mode-toggle")
+
+// check if darkmode is enabled
+// if it`s enabled, turn it off
+// if it`s disabled turn it on
+
+const enableDarkmode = () => {
+    // 1. add the class darkmode to body
+    document.body.classList.add("darkmode");
+    // 2. update darkMode in the local storage
+    localStorage.setItem("darkMode", "enabled");
+};
+const disableDarkmode = () => {
+    // 1. add the class darkmode to body
+    document.body.classList.remove("darkmode");
+    // 2. update darkMode in the local storage
+    localStorage.setItem("darkMode", null);
+};
+
+if (darkMode === "enabled") {
+    enableDarkmode();
+}
+
+darkModeToggle.addEventListener("click", () =>{
+    darkMode = localStorage.getItem("darkMode");
+   if (darkMode !== "enabled") {
+    enableDarkmode();
+    console.log(darkMode);
+   } else {
+    disableDarkmode ();
+    console.log(darkMode);
+   }
+})
